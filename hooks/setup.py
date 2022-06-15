6095304a99c0b3d1469a344ffc611f1c27df42dd
Paul Larson <paul.larson@canonical.com>
2022-06-15 16:16:06 -0500

    Support jammy - fix package name for python3-pip in setup.py

--- hooks/setup.py
+++ hooks/setup.py
@@ -18,7 +18,7 @@ def _install_python_requirements():
     deps = os.path.join(current_dir, 'deps')
     requirements = os.path.join(current_dir, 'requirements.pip')
     subprocess.check_call(['apt-get', 'update'])
-    subprocess.check_call(['apt-get', 'install', '-y', 'python-pip'])
+    subprocess.check_call(['apt-get', 'install', '-y', 'python3-pip'])
     subprocess.check_call([
         'pip', 'install', '--no-index', '--no-dependencies',
         '--find-links', 'file:///{}'.format(deps), '-r', requirements])
